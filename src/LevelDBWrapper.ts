a393e68d1ac7e40ea540116b22de60b494f2eb26
RetoRuto9900K <41989402+tutinoko2048@users.noreply.github.com>
2024-02-05 10:33:29 +0000

    feat: initial buffer key support

--- src/LevelDBWrapper.ts
+++ src/LevelDBWrapper.ts
@@ -1,7 +1,39 @@
 import { existsSync } from 'fs';
 import { LevelDB } from 'leveldb-zlib';
 import { resolve } from 'path';
-import * as nbt from 'prismarine-nbt';
+import * as nbt from 'prismarine-nbt'; 
+import { LevelKey } from './LevelKey';
+
+// Microsoft Creator Docs
+// https://learn.microsoft.com/en-us/minecraft/creator/documents/actorstorage
+// minecraft-creator-tools/WorldChunk/addKeyValue
+// https://github.com/Mojang/minecraft-creator-tools/blob/main/app/src/minecraft/WorldChunk.ts#L138
+export enum LevelChunkTag {
+  Data3D = 43,
+  Version, // This was moved to the front as needed for the extended heights feature. Old chunks will not have this data.
+  Data2D = 45,
+  Data2DLegacy = 46,
+  SubChunkPrefix = 47,
+  LegacyTerrain = 48,
+  BlockEntity = 49,
+  Entity = 50, // Legacy actor storage which will be deleted from LevelDB upon upgrading the chunk to use modern actor storage
+  PendingTicks = 51,
+  LegacyBlockExtraData = 52,
+  BiomeState = 53,
+  FinalizedState = 54,
+  ConversionData = 55, // Data that the converter provides that are used at runtime for things like blending
+  BorderBlocks = 56,
+  HardcodedSpawners = 57,
+  RandomTicks = 58,
+  CheckSums = 59,
+  GenerationSeed = 60,
+  GeneratedPreCavesAndCliffsBlending = 61,
+  BlendingBiomeHeight = 62,
+  MetadataHash = 63,
+  BlendingData = 64,
+  ActorDigestVersion = 65,
+  LegacyVersion = 118
+};
 
 export class LevelDBWrapper {
   public readonly levelDB: LevelDB;
@@ -23,11 +55,21 @@ export class LevelDBWrapper {
     return parsed;
   }
 
-  async getAllKeys(): Promise<string[]> {
+  async getAllKeys(keyAsBuffer?: false): Promise<string[]>;
+  async getAllKeys(keyAsBuffer: true): Promise<Buffer[]>;
+  async getAllKeys(keyAsBuffer: boolean = false): Promise<string[] | Buffer[]> {
     if (!this.levelDB.isOpen()) await this.levelDB.open();
     const keys = [];
-    const iterator = this.levelDB.getIterator({ keyAsBuffer: false });
+    const iterator = this.levelDB.getIterator({ keyAsBuffer });
     for await (const [key] of iterator) keys.push(key);
+    return keys;
+  }
+
+  async getAllKeysRaw(): Promise<LevelKey[]> {
+    if (!this.levelDB.isOpen()) await this.levelDB.open();
+    const keys: LevelKey[] = [];
+    const iterator = this.levelDB.getIterator({ keyAsBuffer: true });
+    for await (const [key] of iterator) keys.push(new LevelKey(key));
     return keys;
   }
 
