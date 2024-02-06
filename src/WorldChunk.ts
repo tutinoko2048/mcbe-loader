09a6f2cc6d2230b72543b40c7ff6d117ace607f5
RetoRuto9900K <41989402+tutinoko2048@users.noreply.github.com>
2024-02-06 11:31:14 +0000

    refactor: move WorldChunkTag to WorldChunk.ts

--- src/WorldChunk.ts
+++ src/WorldChunk.ts
@@ -1,5 +1,35 @@
-import { WorldChunkTag } from './LevelDBWrapper';
 import type { LevelKey } from './LevelKey';
+
+// Microsoft Creator Docs
+// https://learn.microsoft.com/en-us/minecraft/creator/documents/actorstorage
+// minecraft-creator-tools/WorldChunk/addKeyValue
+// https://github.com/Mojang/minecraft-creator-tools/blob/main/app/src/minecraft/WorldChunk.ts#L138
+export enum WorldChunkTag {
+  Data3D = 43,
+  Version = 44, // This was moved to the front as needed for the extended heights feature. Old chunks will not have this data.
+  Data2D = 45,
+  Data2DLegacy = 46,
+  SubChunkPrefix = 47,
+  LegacyTerrain = 48,
+  BlockEntity = 49,
+  Entity = 50, // Legacy actor storage which will be deleted from LevelDB upon upgrading the chunk to use modern actor storage
+  PendingTicks = 51,
+  LegacyBlockExtraData = 52,
+  BiomeState = 53,
+  FinalizedState = 54,
+  ConversionData = 55, // Data that the converter provides that are used at runtime for things like blending
+  BorderBlocks = 56,
+  HardcodedSpawners = 57,
+  RandomTicks = 58,
+  CheckSums = 59,
+  GenerationSeed = 60,
+  GeneratedPreCavesAndCliffsBlending = 61,
+  BlendingBiomeHeight = 62,
+  MetadataHash = 63,
+  BlendingData = 64,
+  ActorDigestVersion = 65,
+  LegacyVersion = 118
+}
 
 interface ChunkKeyInfo {
   tag: WorldChunkTag;
